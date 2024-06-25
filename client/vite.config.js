df146eef90a714e90a42b0d17b37d3145c271280
Naga <nptbilla@gmail.com>
2024-06-25 16:50:01 +0530

    Pushing to Git

---
+++ client/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { fileURLToPath, URL } from "url"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        port: 3000,
+    },
+    build: {
+        chunkSizeWarningLimit: 2500,
+    },
+    resolve: {
+        alias: [
+            {
+                find: "@",
+                replacement: fileURLToPath(new URL("./src", import.meta.url)),
+            },
+        ],
+    },
+})
